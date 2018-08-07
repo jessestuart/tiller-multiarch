36e2dcb9164168b1fda9ad498cb359e85f762050
Jesse Stuart <hi@jessestuart.com>
2018-08-06 23:33:34 -0400

    [commit message skipped]

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-echo 'export DIR=`pwd`' >> $BASH_ENV
-echo 'export QEMU_VERSION=v2.12.0' >> $BASH_ENV
-echo 'export GITHUB_REPO=kubernetes/helm' >> $BASH_ENV
-echo 'export GO_REPO=k8s.io/helm' >> $BASH_ENV
-echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | jq -r "sort_by(.tag_name)[-1].tag_name")' >> $BASH_ENV
+echo 'export DIR=`pwd`' >>$BASH_ENV
+echo 'export QEMU_VERSION=v2.12.0' >>$BASH_ENV
+echo 'export GITHUB_REPO=helm/helm' >>$BASH_ENV
+echo 'export GO_REPO=k8s.io/helm' >>$BASH_ENV
+echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | jq -r "sort_by(.tag_name)[-1].tag_name")' >>$BASH_ENV
 
-echo 'export GOPATH=/root/go' >> $BASH_ENV
-echo 'export GOROOT=/usr/local/go' >> $BASH_ENV
+echo 'export GOPATH=/root/go' >>$BASH_ENV
+echo 'export GOROOT=/usr/local/go' >>$BASH_ENV
 
-echo 'export IMAGE=tiller' >> $BASH_ENV
-echo 'export PROJECT_PATH=$GOPATH/src/$GO_REPO' >> $BASH_ENV
-echo 'export REGISTRY=jessestuart' >> $BASH_ENV
-echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${GOARCH}"' >> $BASH_ENV
+echo 'export IMAGE=tiller' >>$BASH_ENV
+echo 'export PROJECT_PATH=$GOPATH/src/$GO_REPO' >>$BASH_ENV
+echo 'export REGISTRY=jessestuart' >>$BASH_ENV
+echo 'export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${GOARCH}"' >>$BASH_ENV
 
-echo 'export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"' >> $BASH_ENV
+echo 'export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"' >>$BASH_ENV
 
 source $BASH_ENV
--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 ARG target
 
-FROM alpine:3.7 as certs
+FROM alpine:3.8 as certs
 # hadolint ignore=DL3018
 RUN apk add --no-cache ca-certificates
 
-FROM $target/alpine:3.7
+FROM $target/alpine:3.8
 LABEL maintainer="Jesse Stuart <hi@jessestuart.com>"
 COPY --from=certs /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/ca-certificates.crt
 
