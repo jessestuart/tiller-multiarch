0d6f2c05ee69af2aed1ce5ba3e7680549727628c
Jesse Stuart <hi@jessestuart.com>
2018-10-31 08:54:32 -0400

    fix(CI): Correctly determine version number.

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo 'export DIR=`pwd`' >>$BASH_ENV
-echo 'export QEMU_VERSION=v2.12.0' >>$BASH_ENV
+echo 'export QEMU_VERSION=v3.0.0' >>$BASH_ENV
 echo 'export GITHUB_REPO=helm/helm' >>$BASH_ENV
 echo 'export GO_REPO=k8s.io/helm' >>$BASH_ENV
-echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | jq -r "sort_by(.tag_name)[-1].tag_name")' >>$BASH_ENV
+echo 'export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | jq -r ".[].tag_name" | sort -Vr | head -n1)' >>$BASH_ENV
 
 echo 'export GOPATH=/root/go' >>$BASH_ENV
 echo 'export GOROOT=/usr/local/go' >>$BASH_ENV
