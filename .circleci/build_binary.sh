a0666ea182607549011f30004543cdccf8311732
Jesse Stuart <hi@jessestuart.com>
2019-02-25 02:17:30 -0500

    [commit message skipped]

--- .circleci/build_binary.sh
+++ .circleci/build_binary.sh
@@ -4,22 +4,10 @@ set -eu
 
 cd "$PROJECT_PATH" || exit 1
 
-# Go won't let us cross-compile if GOBIN is set.
-# shellcheck disable=SC2016
-# sed -e 's/GOBIN=$(BINDIR) //' -i Makefile
 sed -e "s/^TARGETS.*$/TARGETS \?\= linux\/${_GOARCH}/" -i Makefile
-
-echo '==============='
-command -v go
-echo '==============='
 
 docker run --rm --privileged multiarch/qemu-user-static:register
 make build-cross
 
-cp "$PROJECT_PATH/_dist/linux-${_GOARCH}/tiller" .
 cp "$PROJECT_PATH/_dist/linux-${_GOARCH}/tiller" "${CIRCLE_WORKING_DIRECTORY}"
 cp "$PROJECT_PATH/_dist/linux-${_GOARCH}/helm" "${CIRCLE_WORKING_DIRECTORY}"
-# if test "$GOARCH" == 'amd64'; then
-#   cp "$(command -v tiller)" "$DIR"
-# else
-# fi
