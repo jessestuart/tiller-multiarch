9fc9be15562d29b0c53b357e8e30857b8e80ab73
Jesse Stuart <hi@jessestuart.com>
2019-02-25 02:00:52 -0500

    [commit message skipped]

--- .circleci/build_binary.sh
+++ .circleci/build_binary.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -eu
+
 cd "$PROJECT_PATH" || exit 1
 
 # Go won't let us cross-compile if GOBIN is set.
 # shellcheck disable=SC2016
-sed -e 's/GOBIN=$(BINDIR) //' -i Makefile
+# sed -e 's/GOBIN=$(BINDIR) //' -i Makefile
 sed -e "s/^TARGETS.*$/TARGETS \?\= linux\/${GOARCH}/" -i Makefile
 
 docker run --rm --privileged multiarch/qemu-user-static:register
