0aae05c900ee7faa92b3b6796449af1f636c715c
Jesse Stuart <hi@jessestuart.com>
2019-05-13 15:17:51 -0400

    feat(deps): Bump qemu & golang versions.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- .circleci/load_env.sh
+++ .circleci/load_env.sh
@@ -2,7 +2,7 @@
 
 echo '
 export DIR=`pwd`
-export QEMU_VERSION=v3.0.0
+export QEMU_VERSION=v4.0.0
 export GITHUB_REPO=helm/helm
 export GO_REPO=k8s.io/helm
 export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
@@ -13,7 +13,7 @@ export IMAGE=tiller
 export PROJECT_PATH=$GOPATH/src/$GO_REPO
 export REGISTRY=jessestuart
 export IMAGE_ID="${REGISTRY}/${IMAGE}:${VERSION}-${_GOARCH}"
-export PATH="$GOROOT/bin:$HOME/go/bin:$PATH"
+export PATH="$GOROOT/bin:$GOPATH/bin:$PATH"
 ' >>$BASH_ENV
 
 source $BASH_ENV
