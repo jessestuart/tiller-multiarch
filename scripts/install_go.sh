d478a645c344ba66e29d52653fa6f5bdd04c2688
Jesse Stuart <hi@jessestuart.com>
2019-09-11 06:07:38 -0400

    feat(deps): Bump go version to 1.13.
    Signed-off-by: Jesse Stuart <hi@jessestuart.com>

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -7,6 +7,6 @@ test -d /lib64 || mkdir /lib64
 ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 
 rm -rf /usr/local/go
-curl -sL https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz | tar xz
+curl -sL https://dl.google.com/go/go1.13.linux-amd64.tar.gz | tar xz
 mv go/ /usr/local/
 go env && go version
