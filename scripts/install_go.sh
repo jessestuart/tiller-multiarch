bd110425b976edfd03d965f526a27e5d24d4e1fb
Jesse Stuart <hi@jessestuart.com>
2019-09-18 00:51:56 -0400

    fix(ci): Fix curl.

--- scripts/install_go.sh
+++ scripts/install_go.sh
@@ -9,4 +9,7 @@ ln -s /lib/libc.musl-x86_64.so.1 /lib64/ld-linux-x86-64.so.2
 rm -rf /usr/local/go
 curl -sL https://dl.google.com/go/go1.13.linux-amd64.tar.gz | tar xz
 mv go/ /usr/local/
+
+export GOROOT=/usr/local/go/
+
 go env && go version
