d81ffee70e19110b309be6d5572217ddb42e7cfc
Jesse Stuart <hi@jessestuart.com>
2019-11-06 15:05:36 -0500

    [commit message skipped]

--- scripts/install_glide.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-curl -sL https://github.com/Masterminds/glide/releases/download/v0.13.3/glide-v0.13.3-linux-amd64.tar.gz | tar xz
-chmod +x linux-amd64/glide
-mv linux-amd64/glide /usr/bin/glide
