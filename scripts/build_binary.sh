6dd7c57807920561fc269c0752a80fb1b0b34149
Jesse Stuart <hi@jessestuart.com>
2019-11-20 05:36:21 -0500

    [commit message skipped]

--- scripts/build_binary.sh
+++ scripts/build_binary.sh
@@ -2,11 +2,7 @@
 
 set -eu
 
-cd "$PROJECT_PATH" || exit 1
-
 docker run --rm --privileged multiarch/qemu-user-static:register
-
-mkdir -p "$PROJECT_PATH"
 
 git clone "https://github.com/${GITHUB_REPO}" --depth=1 "$PROJECT_PATH"
 cd "$PROJECT_PATH"
