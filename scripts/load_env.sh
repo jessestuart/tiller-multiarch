9fb039ae88cdd9944582b15a580ba2c2f01e4a77
Jesse Stuart <hi@jessestuart.com>
2020-03-16 20:51:14 -0400

    fix(ci): Correctly tag image versions.
    i.e., versions should be locked to the v2 upstream.

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -5,7 +5,12 @@ export DIR=`pwd`
 export QEMU_VERSION=v4.0.0
 export GITHUB_REPO=helm/helm
 export GO_REPO=k8s.io/helm
-export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases/latest | jq -r ".tag_name")
+export VERSION=$(curl -s https://api.github.com/repos/${GITHUB_REPO}/releases | \
+  jq -r "sort_by(.tag_name) | .[].tag_name" | \
+  grep -v v3 | \
+  sort -V -r | \
+  head -n1 \
+)
 
 export GOPATH="$HOME/go"
 export GOROOT=/usr/local/go
